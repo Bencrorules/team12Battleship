004c52875661e76bff6bd29a313112dc9a6a1120
aldeguersam@gmail.com <aldeguersam@ku.edu>
2022-02-08 00:12:54 -0600

    Change method name buildBoard() to playGame() and add player attributes

--- game.h
+++ game.h
@@ -1,14 +1,20 @@
 #ifndef GAME_H
 #define GAME_H
+#include "player.h"
 
 class Game
 {
-public:
+    public:
+    Game();
+    void playGame();
     void makeBoard();
-    void printBoard(int **arr);
-    void boardBuild();
-    int **board;
-    int row = 10;
-    int col = 10;
+    void printBoard();
+    void obtainShips();
+    int** board;
+    int row;
+    int col;
+    int shipAmount;
+    Player* player1;
+    Player* player2;
 };
 #endif
