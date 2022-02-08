a82cce117deaae4884c4bb1ccf2b0252ad7bee21
aldeguersam@gmail.com <aldeguersam@ku.edu>
2022-02-08 00:13:21 -0600

    Wrote beginning of game in playGame() method and began working on helper method obtainShips()

--- game.cpp
+++ game.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include "game.h"
 
+Game::Game()
+{
+    makeBoard();
+    row = 10;
+    col = 10;
+    shipAmount = 0;
+    player1 = nullptr;
+    player2 = nullptr;
+}
+
 void Game::makeBoard()
 {
     board = new int *[row];
@@ -18,19 +28,38 @@ void Game::makeBoard()
     }
 }
 
-void Game::printBoard(int **arr)
+void Game::printBoard()
 {
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
         {
-            std::cout << arr[i][j] << " ";
+            std::cout << board[i][j] << " ";
         }
-        std::cout << '\n';
+        std::cout << std::endl;
     }
 }
 
-void Game::boardBuild(){
-    makeBoard();
-    printBoard(board);
+void Game::playGame()
+{
+    std::cout << "Welcome to Battleship!" << std::endl;
+    std::cout << "----------------------" << std::endl;
+    do {
+        std::cout << "Ship amount: ";
+        std::cin >> shipAmount;
+        if (shipAmount < 1 || shipAmount > 5)
+        {
+            std::cout << "Ship amount must be between 1-5." << std::endl;
+        }
+    } while(shipAmount < 1 || shipAmount > 5);
+    Player* player1 = new Player(shipAmount);
+    Player* player2 = new Player(shipAmount);
+    obtainShips();
+
+
+}
+
+void Game::obtainShips()
+{
+    printBoard();
 }
