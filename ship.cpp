0ad2582f08059f9fa0c526df7f2dd141c72d818d
aldeguersam@gmail.com <aldeguersam@ku.edu>
2022-02-07 23:09:48 -0600

    Create .cpp file for Ship class

---
+++ ship.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "ship.h"
+
+Ship::Ship(int length)
+{
+    shipCoords = new int*[length];
+    for (int i = 0; i < length; i++)
+    {
+        shipCoords[i] = new int[2];
+    }
+    sank = false;
+}
