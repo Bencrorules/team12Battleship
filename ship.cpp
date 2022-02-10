fa7715c6588c83799fb219e5f81bff28a2a85d49
aldeguersam@gmail.com <aldeguersam@ku.edu>
2022-02-10 01:14:56 -0600

    Implement setXCoord and setYCoord methods

--- ship.cpp
+++ ship.cpp
@@ -4,9 +4,11 @@
 Ship::Ship(int length)
 {
     shipCoords = new int*[length];
+    spotsHit = new bool[length];
     for (int i = 0; i < length; i++)
     {
         shipCoords[i] = new int[2];
+        spotsHit[i] = false;
     }
     sank = false;
 }
@@ -14,4 +16,14 @@ Ship::Ship(int length)
 bool Ship::isSank()
 {
     return sank;
+}
+
+void Ship::setXCoord(int spotNumber, char value)
+{
+    shipCoords[spotNumber][0] = value;
+}
+
+void Ship::setYCoord(int spotNumber, int value)
+{
+    shipCoords[spotNumber][1] = value;
 }
