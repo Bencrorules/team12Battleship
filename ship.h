a00508018837c2d35c7babf1258ebd5039adbdb7
aldeguersam@gmail.com <aldeguersam@ku.edu>
2022-02-10 01:14:17 -0600

    Add setXCoord and setYCoord methods, as well as spotsHit array indicating which spots have been hit

--- ship.h
+++ ship.h
@@ -6,9 +6,12 @@ class Ship
     public:
     Ship(int length);
     bool isSank();
+    void setXCoord(int spotNumber, char value);
+    void setYCoord(int spotNumber, int value);
 
     private:
     int** shipCoords;
+    bool* spotsHit;
     bool sank;
 };
 #endif
