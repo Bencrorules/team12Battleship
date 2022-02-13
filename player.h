d462ac021c9cd46f5fd6e2fcbdbd122f991dd20a
Ben Crocker <bencrorules@gmail.com>
2022-02-13 15:52:36 -0600

    Added comments for player class and header

--- player.h
+++ player.h
@@ -5,23 +5,21 @@
 class Player
 {
     public:
-    Player(int amountShips);
-    ~Player();
-    void addShip(int shipNumber, Ship* ship);
+    Player(int amountShips); //Player constructor with ship amount as an integer input
+    ~Player(); //Player destructor
+    void addShip(int shipNumber, Ship* ship); //addShip function with shipNumber and ship pointer
     
-    void addShip(int shipNumber, int coordIndex, char xCoord, int yCoord, int shipSize);
+    void addShip(int shipNumber, int coordIndex, char xCoord, int yCoord, int shipSize); //addShip function with shipNumber and its data manually
     void getShipInfo(int shipNumber, int coordIndex); // Temp line - Lee
-    Ship* getShip(int shipNumber);
-    int getPrevXCoord(int shipNumber, int coordIndex);
-    int getPrevYCoord(int shipNumber, int coordIndex);
+    Ship* getShip(int shipNumber); //returns the ship pointer when given the shipNumber
+    int getPrevXCoord(int shipNumber, int coordIndex); //returns the previous X coordinate when given the shipNumber and the coordinate index
+    int getPrevYCoord(int shipNumber, int coordIndex); //returns the previous Y coordinate when given the shipNumber and the coordinate index
 
-    bool shipAttacked(char xGuess, int yGuess);
-    bool allShipDown();
-
-
+    bool shipAttacked(char xGuess, int yGuess); //returns a bool if the given coordinate matches the ships
+    bool allShipDown(); //returns a bool if all ships are sunk
 
     private:
-    Ship** ships;
-    int numberOfShips;
+    Ship** ships; //declares a ship pointer 'ships'
+    int numberOfShips; //declares an int 'numberOfShips'
 };
 #endif
